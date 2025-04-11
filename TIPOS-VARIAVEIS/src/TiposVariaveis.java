a3d92d71ac64f75e0fd0c4c93cc28fcc9da5deac
Hernan Polo <hernan.polo1985@gmail.com>
2025-04-10 21:18:50 -0300

    concluindo o modulo tipos e variaveis

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       double salarioMinimo = 2500;
+//tipos primitivos
+//estudem a classe String que representa texto na aplicação
+
+       String meuNomeCompleto = "Hernán Osvaldo Polo";
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal; 
+    
+       final int NUMERO = 5;
+
+       NUMERO = 10; 
+      System.out.println(NUMERO);
+
     }
 }
